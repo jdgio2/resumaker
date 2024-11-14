a20d764adab92de5c388d34ce1c31d4ae1e7dc20
JD Giovannetti <jonathangiovannetti2@gmail.com>
2024-11-13 16:43:14 -0800

    Basic state functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import './App.css'
-import ResumePreview from './components/ResumePreview'
-import InputField from './components/InputField';
-
+import "./App.css";
+import ResumePreview from "./components/ResumePreview";
+import InputField from "./components/InputField";
+import { useState } from "react";
 
 function App() {
+  const [resumeHeader, setResumeHeader] = useState({
+    first_name: "Test First Name",
+    last_name: "Test Last Name",
+    links: [
+      { href: "google.com", name: "Google" },
+      { href: "amazon.com", name: "Amazon" },
+    ],
+  });
+  const [resumeExperience, setResumeExperience] = useState({
+    role: "Test Role 1",
+    company: "Test Company 1",
+    years: "2000-2001",
+    info: "At this Company 1, I did such and such a thing at such and such a place which resulted in much profit and joy for everyone involved.",
+  });
+  const [resumeEducation, setResumeEducation] = useState({
+    degree: "Test Degree 1",
+    university: "Test University 1",
+    attended: "2000-2001",
+  });
+  const [resumeProjects, setResumeProjects] = useState({
+    title: "Project Title 1",
+    info: "On this project, I utilized, spearheaded, and formulated the most amazizng thing every imagined by man so that many people were happy. We accumulated 4000 users in one nanosecond.",
+  });
   return (
     <div className="parent-div">
-      <InputField></InputField>
-      <ResumePreview></ResumePreview>
+      <InputField
+        handleHeader={(new_info) => {
+          setResumeHeader(new_info);
+        }}
+        handleBody={(new_info) => {
+          setResumeExperience(new_info);
+        }}
+        handleExperience={(new_info) => {
+          setResumeEducation(new_info);
+        }}
+        handleEducation={(new_info) => {
+          setResumeProjects(new_info);
+        }}
+      ></InputField>
+      <ResumePreview
+        header={resumeHeader}
+        body={resumeExperience}
+        education={resumeEducation}
+        projects={resumeProjects}
+      ></ResumePreview>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -1,40 +1,51 @@
-import '../App.css'
-import '../index.css'
+import "../App.css";
+import "../index.css";
 
-
-export default function ResumePreview() {
-    return(
+// { header, body, education, projects }
+export default function ResumePreview({ header, body, education, projects }) {
+  return (
     <div className="resume-preview">
-        <div className="resume-header">
-            <h1>
-                <strong className="bold-name">First Name</strong> Last Name
-            </h1>
-            <div className="resume-links">
-                <span>Link 1</span> <span>Link 2</span>
-            </div>
+      <div className="resume-header">
+        <h1>
+          <strong className="bold-name">{header.first_name}</strong>{" "}
+          {header.last_name}
+        </h1>
+        <div className="resume-links">
+          {header.links.map((linkInfo) => (
+            <a key={linkInfo.name} href={linkInfo.href}>
+              {linkInfo.name}
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <div className="resume-body">
+        <div className="resume-experience">
+          <h3>Experience</h3>
+          <p>
+            <span className="colored-role">{body.role}</span> | {body.company}
+          </p>
+          <p className="experience-years">{body.years}</p>
+          <p className="experience-accomplishments">{body.info}</p>
         </div>
 
-        <div className="resume-body">
-            <div className="resume-experience">
-                <h3>Experience</h3>
-                <p><span className="colored-role">Role</span> | Company</p>
-                <p className="experience-years">Years</p>
-                <p className="experience-accomplishments">Accomplishments</p>
-            </div>
-
-            <div className="resume-education">
-                <h3>Education</h3>
-                <p><span className="colored-major">Major</span> | University</p>
-                <p className="education-years">Years Attended</p>
-            </div>
-
-            <div className="resume-projects">
-                <h3>Projects</h3>
-                <p><span className="colored-project-title">Project Title</span></p>
-                <p className="project-info">Project Info</p>
-            </div>
+        <div className="resume-education">
+          <h3>Education</h3>
+          <p>
+            <span className="colored-major">{education.degree}</span> |{" "}
+            {education.university}
+          </p>
+          <p className="education-years">{education.attended}</p>
         </div>
+
+        <div className="resume-projects">
+          <h3>Projects</h3>
+          <p>
+            <span className="colored-project-title">{projects.title}</span>
+          </p>
+          <p className="project-info">{projects.info}</p>
+        </div>
+      </div>
     </div>
-
-    );
+  );
 }
